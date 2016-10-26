0663e94078b1e13dddf04982ef373e38d57092db
Peter Kupietz <p.kupietz@socoto.com>
2016-10-26 13:56:46 +0200

    Fixed mission colon

--- flow/leaflet/LineUtil.js
+++ flow/leaflet/LineUtil.js
@@ -2,5 +2,5 @@ declare interface LeafletLineUtil {
     simplify(points: Array<LeafletPoint>, tolerance: number): Array<LeafletPoint>,
     pointToSegmentDistance(p: LeafletPoint, p1: LeafletPoint, p2: LeafletPoint): number,
     closestPointOnSegment(p: LeafletPoint, p1: LeafletPoint, p2: LeafletPoint): number,
-    clipSegment(a: LeafletPoint, b: LeafletPoint, bounds: LeafletBounds, useLastCode?: boolean, round?: boolean) Array<LeafletPoint> | boolean
+    clipSegment(a: LeafletPoint, b: LeafletPoint, bounds: LeafletBounds, useLastCode?: boolean, round?: boolean): Array<LeafletPoint> | boolean
 }
